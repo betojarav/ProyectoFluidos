4322da74aa6ee2b938823cfa3766a2bd1b1fb1ab
betojarav <aajara4@uc.cl>
2020-11-25 13:08:52 -0400

    se crean las vistas, falta controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  # Rutas instrumentos
+  get 'instrumentos/barometro'
+  get 'instrumentos/vacuometro'
+  get 'instrumentos/piezometro'
+  # Rutas tipos de presion
+  get 'presion/vacio'
+  get 'presion/relativa'
+  get 'presion/atmosferica'
 end
